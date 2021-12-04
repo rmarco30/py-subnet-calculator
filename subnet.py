e9befb3787b4bc9116e6c5dd1c740a3287a7ee56
rmarco30 <marcoroldanl1130@gmail.com>
2021-12-04 14:34:23 +0800

    subnet calculator v2

--- subnet.py
+++ subnet.py
@@ -1,166 +1,141 @@
 import math
 
-# ip = '172.200.0.0'
-# host = 1000
 
+class Network:
+	def __init__(self, ip, host):
 
-def solve(ip, host):
-
-	print(bits(host, 'str'))
-	print(cidr('str'))
-	print(increment('str'))
-	print(subnet('dec'))
-	print(first_nw(ip, 'str'))
-	print(range_nw())
-	print(broadcast('str'))
-	print(next_nw('str'))
+		self.ip = ip
+		self.host = int(host, 10)
 
 
 
-def bits(host, opt = 'int'):
+	def bits(self):
 
-	for bits in range(32):
+		for bits in range(32):
+			if self.host <= 2**(bits):
+				break
 
-		if host <= 2**(bits):
-			break
-
-	if opt == 'str':
-		return str(bits) + ' bits'
-
-	else:
 		return bits
 
 
-def cidr(host, opt = 'int'):
 
-	if opt == 'str':
-		return '/' + str(32 - bits(host))
+	def cidr(self):
 
-	else:
-		return 32 - bits(host)
+		return 32 - self.bits()
 
 
-def increment(host, opt = 'int'):
 
-	inc = [128, 64, 32, 16, 8, 4, 2, 1]
+	def octet(self):
 
-	octet = math.ceil(cidr(host) / 8)
-	increment = inc[(cidr(host) % 8) - 1]
+		octet = math.ceil(self.cidr() / 8)
 
-	if(octet == 1):
-		ordinal = 'st'
-	elif(octet == 2):
-		ordinal = 'nd'
-	elif(octet == 3):
-		ordinal = 'rd'
-	else:
-		ordinal = 'th'
+		if(octet == 1):
+			ordinal = 'st'
+		elif(octet == 2):
+			ordinal = 'nd'
+		elif(octet == 3):
+			ordinal = 'rd'
+		else:
+			ordinal = 'th'
 
-	if opt == 'str':
+		return str(octet) + ordinal
 
-		s = str(octet) + ordinal, str(increment) + 'i'
-		return str(s).replace("\'", '')
 
-	else:
+
+	def increment(self):
+
+		inc = [128, 64, 32, 16, 8, 4, 2, 1]
+
+		increment = inc[(self.cidr() % 8) - 1]
+
 		return increment
 
 
-def subnet(host, inwhat):
 
-	snm_dec = ['','','','',]
-	snm_bin = ['','','','',]
-	snm = ''
-	x = 0
-	
-	for i in range(32):
+	def subnet(self):
 
-		if i < math.floor(cidr(host)):
-			snm += '1'
+		snm_dec = ['','','','',]
+		snm_bin = ['','','','',]
+		snm = ''
+		x = 0
+		
+		for i in range(32):
 
-		elif i >= math.floor(cidr(host)):
-			snm += '0'
+			if i < math.floor(self.cidr()):
+				snm += '1'
 
-		if (i+1) % 8 == 0:
-			snm += '.'
-			snm_bin[x] = snm[:-1]
-			snm_dec[x] = int(snm_bin[x], 2)
-			snm = ''
-			x += 1
+			elif i >= math.floor(self.cidr()):
+				snm += '0'
 
-	if inwhat == 'dec':
-		return '.'.join(map(str, snm_dec)) + '\n'
-	if inwhat == 'bin':
-		return '.'.join(map(str, snm_bin)) + '\n'
+			if (i+1) % 8 == 0:
+				snm += '.'
+				snm_bin[x] = snm[:-1]
+				snm_dec[x] = int(snm_bin[x], 2)
+				snm = ''
+				x += 1
+
+		return snm_dec
 
 
-def first_nw(ip, opt = 'int'):
+
+	def firstNw(self):
 
 		first_nw = ['','','','']
 
-		num_per_octet = ip.split('.')
+		num_per_octet = self.ip.split('.')
 
 		for x, num in enumerate(num_per_octet):
 			first_nw[x] = int(num, 10)
 
-		
-		if opt == 'str':
-			return '.'.join(map(str, first_nw))
+		return first_nw
+
+
+
+	def nextNw(self):
+
+		next_nw = list(self.firstNw())
+		inc = 0
+		new_value = 0
+
+		octet_to_change = math.ceil(self.cidr() / 8) - 1
+
+		new_value = next_nw[octet_to_change] + self.increment()
+
+		if new_value == 256:
+			next_nw[octet_to_change] = 0
+			next_nw[octet_to_change - 1] += 1
 
 		else:
-			return first_nw
+			next_nw[octet_to_change] = new_value
 
-
-def next_nw(ip, host, opt = 'int'):
-
-	next_nw = list(first_nw(ip))
-	inc = 0
-	new_value = 0
-
-	octet_to_change = math.ceil(cidr(host) / 8) - 1
-
-	new_value = next_nw[octet_to_change] + increment(host)
-
-	if new_value == 256:
-		next_nw[octet_to_change] = 0
-		next_nw[octet_to_change - 1] += 1
-
-	else:
-		next_nw[octet_to_change] = new_value
-
-	if opt == 'str':
-		return '.'.join(map(str, next_nw))
-
-	else:
 		return next_nw
 
 
-def broadcast(ip, host, opt = 'int'):
 
-	broadcast = list(next_nw(ip, host))
-	new_value = 0
+	def broadcast(self):
 
-	new_value = broadcast[3] - 1
+		broadcast = list(self.nextNw())
+		new_value = 0
 
-	if new_value == -1:
-		broadcast[2] -= 1
-		broadcast[3] = 255
+		new_value = broadcast[3] - 1
 
-	else:
-		broadcast[3] -= 1
+		if new_value == -1:
+			broadcast[2] -= 1
+			broadcast[3] = 255
 
-	if opt == 'str':
-		return '.'.join(map(str, broadcast))
+		else:
+			broadcast[3] -= 1
 
-	else:
 		return broadcast
 
 
-def range_nw(ip, host):
 
-	first_usable = list(first_nw(ip))
-	last_usable = list(broadcast(ip, host))
+	def rangeNw(self):
 
-	first_usable[3] += 1
-	last_usable[3] -= 1
+		first_usable = list(self.firstNw())
+		last_usable = list(self.broadcast())
 
-	return str(first_usable[2]) + '.' + str(first_usable[3]) + ' - ' +  str(last_usable[2]) + '.' + str(last_usable[3])
+		first_usable[3] += 1
+		last_usable[3] -= 1
+
+		return f"{first_usable[2]}.{first_usable[3]} - {last_usable[2]}.{last_usable[3]}"
